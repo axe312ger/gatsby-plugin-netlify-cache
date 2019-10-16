ce4fd7c63b53415f04cb912afb5382d14ad6bbde
Derek Nguyen <derek@penandpillow.com>
2019-10-16 12:49:15 +0900

    use gatsby reporter instead of console.log

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -45,7 +45,7 @@ function generateCacheDirectoryNames(rootDirectory, netlifyCacheDir, dirPath) {
 }
 
 exports.onPreInit = async function(
-  { store },
+  { store, reporter },
   { extraDirsToCache, cachePublic, verbose = false }
 ) {
   if (!process.env.NETLIFY_BUILD_BASE) {
@@ -81,7 +81,7 @@ exports.onPreInit = async function(
       cacheFileCount = cacheFiles.length
     }
 
-    console.log(
+    reporter.info(
       `plugin-netlify-cache: Restoring ${
         cacheFileCount
       } cached files for ${humanName} directory with ${
@@ -92,11 +92,11 @@ exports.onPreInit = async function(
     await copy(cachePath, dirPath)
   }
 
-  console.log(`plugin-netlify-cache: Netlify cache restored`)
+  reporter.success(`plugin-netlify-cache: Netlify cache restored`)
 }
 
 exports.onPostBuild = async function(
-  { store },
+  { store, reporter },
   { extraDirsToCache, cachePublic }
 ) {
   if (!process.env.NETLIFY_BUILD_BASE) {
@@ -118,10 +118,10 @@ exports.onPostBuild = async function(
       dirPath
     )
 
-    console.log(`plugin-netlify-cache: Caching ${humanName}...`)
+    reporter.info(`plugin-netlify-cache: Caching ${humanName}...`)
 
     await copy(dirPath, cachePath)
   }
 
-  console.log(`plugin-netlify-cache: Netlify cache refilled`)
+  reporter.success(`plugin-netlify-cache: Netlify cache refilled`)
 }
