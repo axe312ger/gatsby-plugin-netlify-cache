573a034d6e0276a8f8345d93bbd7fdd23e600656
Derek Nguyen <derek@penandpillow.com>
2019-10-16 12:46:16 +0900

    readFileCount

---
+++ src/utils.js
@@ -0,0 +1,22 @@
+const fs = require(`fs-extra`)
+const path = require(`path`)
+
+async function readFileCount(targetPath) {
+  const files = await fs.readdir(targetPath)
+  const countP = files.map(async (file) => {
+    const filePath = path.join(targetPath, file)
+    const stats = await fs.stat(filePath)
+    if (stats.isDirectory()) {
+      const count = await readFileCount(filePath)
+      return count
+    } else {
+      return 1
+    }
+  })
+
+  const results = await Promise.all(countP)
+  const total = results.reduce((cur, acc) => acc + cur, 0)
+  return total
+}
+
+exports.readFileCount = readFileCount
